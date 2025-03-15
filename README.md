cc0f248af904d91ae31a2d585fa4e42a7170fa26
Christian Ruettimann <chris@k3s01.sun.bitbull.ch>
2025-03-15 15:11:27 +0100

    first setup

---
+++ README.md
@@ -0,0 +1,12 @@
+# Setup Nocobase on K3S
+## Prepare config
+```
+cp -av install_template.sh myinstall.sh
+vim myinstall.sh
+# edit as you need
+```
+## setup application
+```
+bash myinstall.sh
+```
+
---
+++ install_template.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# nocobase k3s installer
+
+genpasswd() {
+   local l=$1
+   [ "$l" == "" ] && l=32
+   tr -dc A-Za-z0-9 < /dev/urandom | head -c ${l} | xargs
+}
+
+# namespace
+NAMESPACE=nocobase
+
+# Public URL
+HOST=noco.domain.tld
+
+# Generate MySQL password (used for both MYSQL_PASSWORD and SEMAPHORE_DB_PASS)
+MYSQL_PASSWORD=$(genpasswd)
+MYSQL_ROOT_PASSWORD=$(genpasswd)
+
+# Generate access key encryption key
+APP_KEY=$(head -c32 /dev/urandom | base64)
+
+# convert secrets
+MYSQL_PASSWORD_BASE64=$(echo -n "$MYSQL_PASSWORD" | base64)
+MYSQL_ROOT_PASSWORD_BASE64=$(echo -n "$MYSQL_ROOT_PASSWORD" | base64)
+APP_KEY_BASE64=$(echo -n "$APP_KEY" | base64)
+
+test -d runtime && echo ERROR remove ./runtime dir first
+test -d ./runtime && exit 1
+mkdir ./runtime
+chmod 700 ./runtime
+for y in *.yml
+do
+  cp -av $y ./runtime/$y
+  sed -i "s/__NAMESPACE__/$NAMESPACE/g"                                       ./runtime/$y
+  sed -i "s/__HOST__/$HOST/g"                                                 ./runtime/$y
+  sed -i "s/__MYSQL_PASSWORD_BASE64__/$MYSQL_PASSWORD_BASE64/g"               ./runtime/$y
+  sed -i "s/__MYSQL_ROOT_PASSWORD_BASE64__/$MYSQL_ROOT_PASSWORD_BASE64/g"     ./runtime/$y
+  sed -i "s/__APP_KEY_BASE64__/$APP_KEY_BASE64/g"                             ./runtime/$y
+done
+
+echo "# current vars
+NAMESPACE=$NAMESPACE
+HOST=$HOST
+MYSQL_PASSWORD=$MYSQL_PASSWORD
+MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD
+APP_KEY=$APP_KEY
+" > ./runtime/env.sh
+
+ls -l ./runtime
+
+kubectl create namespace $NAMESPACE
+kubectl config set-context --current --namespace=$NAMESPACE
+
+for y in *.yml
+do
+  echo runtime/$y
+  kubectl apply -f runtime/$y
+done
+
+echo "all done, now login as admin:
+Here are te defaults, change it now:
+User name: admin@nocobase.com
+Password: admin123
+url: https://$HOST
+"
